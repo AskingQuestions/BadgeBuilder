bebe0106a891d9b2239ce8ba9f870c892ccb3c13
Jeremy <18402381+AskingQuestions@users.noreply.github.com>
2019-06-18 23:50:40 -0400

    Initial setup.

---
+++ src/vue/app.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="app">
+		
+	</div>
+</template>
+
+<style lang="less">
+	.app {
+		display: flex;
+
+		height: 100%;
+		width: 100%;
+	}
+
+	html, body {
+		margin: 0;
+		height: 100%;
+	}
+</style>
